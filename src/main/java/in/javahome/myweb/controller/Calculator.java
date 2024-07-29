d6c26754f8c56a977b646c06af86fe116f1c26dc
shyamsankarkr <106815168+shyamsankarkr@users.noreply.github.com>
2024-07-29 12:09:15 +0530

    Update Calculator.java

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -9,8 +9,8 @@ public class Calculator {
 	 * @param j
 	 * @return int
 	 */
-	public int add(int i, int j){
-		return i+j;
+	public int add( int j){
+		return j;
 	}
 	public int multiply(int i, int j){
 		return i*j;
